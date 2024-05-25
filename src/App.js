655616ba6fa2e3bbe480eb8bc0b9d4341be45dd2
Shri Srivastava <76124911+shri33@users.noreply.github.com>
2024-05-25 10:17:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">
+          <img src={logo} className="App-logo" alt="logo" />
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
